21c42de3a21158e4a15df477cac7779cbc58d970
Devashish Choubey <devashish.kumar@nslhub.com>
2020-10-30 13:10:36 +0530

    adding new api

--- nslhub/managers/urls.py
+++ nslhub/managers/urls.py
@@ -4,6 +4,7 @@ from managers import views
 urlpatterns = [ 
       url('api/Manager',views.LeaderUndersManager.as_view()),
       url('api/LeaderSkillData',views.LeaderSkillData.as_view()),
+      url('api/Leader',views.LeaderUndersManagerFistnameLastname.as_view()),
     
  ]
 
